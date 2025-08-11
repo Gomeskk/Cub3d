493c4450cf67a1089d8bbc47ee316e4c627410c8
Gomeskk <gomesj778@gmail.com>
2025-08-11 19:11:31 +0100

    code structure

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -3,6 +3,7 @@
 # define CUB3D_H
 
 # include <mlx.h>
+# include <mlx_int.h>
 # include <stdlib.h>
 
 # define HEIGHT 1080
@@ -10,30 +11,62 @@
 # define TILE_SIZE 64
 # define FPS 60
 
-typedef struct s_player
+typedef struct s_tiles
 {
-    double  x;
-    double  y;
-    double  angle;
-    int     turn_dir;   // -1 left, 1 right
-    int     walk_dir;   // -1 back, 1 front
-    double  move_speed;
-    double  rot_speed;
-}   t_player;
+	t_img	wall;
+	t_img	floor;
+}			t_tiles;
 
 typedef struct s_map
 {
-    char    **lines;
-    int     width;
-    int     height;
-}   t_map;
+	char	**map;
+	char	**map_cpy;
+	int		row_count;
+	int		col_count;
+}			t_map;
 
-typedef struct s_game
+typedef struct s_player
 {
-    void    *mlx_ptr; // connection to MLX
-    void    *win_ptr; // window reference
-    t_player player;
-    t_map   map;
-}   t_game;
+	int	pos_y;
+	int	pos_x;
+}			t_player;
+
+typedef struct s_keys
+{
+	int	w;
+	int	a;
+	int	s;
+	int	d;
+}			t_keys;
+
+typedef struct s_mouse
+{
+	int	x;
+	int	y;
+	int	prev_x;
+	int	prev_y;
+	int	delta_x;
+	int	delta_y;
+}			t_mouse; //mouse rotation
+
+typedef enum e_status
+{	
+    MENU,
+	SKIN_SELECT,
+	GAME,
+	CREDITS,
+}			t_status;
+
+typedef struct s_cub3d
+{
+	void		*mlx;
+	void		*window;
+	t_status	status;
+	t_tiles		tiles;
+	t_map		map;
+	t_player	player;
+	t_keys		keys;
+    t_mouse		mouse;
+}			t_cub3d;
 
 #endif
