7bb112a8c1ff1286caff99aced90db5ea97905c1
Gomeskk <gomesj778@gmail.com>
2026-01-11 02:10:14 +0000

    improved put_img_to_img

--- src/menu/menu_transparency.c
+++ src/menu/menu_transparency.c
@@ -3,10 +3,12 @@
 void	pixel_put(t_menu_img *img, int x, int y, int color)
 {
 	char	*pixel;
+	int		offset;
 
 	if (x < 0 || y < 0 || y >= img->height || x >= img->width)
 		return ;
-	pixel = img->data + (y * img->size_line + x * (img->bpp / 8));
+	offset = (img->bpp >> 3);
+	pixel = img->data + (y * img->size_line + x * offset);
 	*(unsigned int *)pixel = color;
 }
 
@@ -14,19 +16,23 @@ void	put_img_to_img(t_menu_img *screen, t_menu_img img, int screen_x, int screen
 {
 	int		x;
 	int		y;
-	char	*color;
+	int		*src_pixel;
+	int		*dst_pixel;
+	int		pixel_offset;
 
-	y = 0;
-	while (y < img.height)
+	pixel_offset = (img.bpp >> 3);
+	y = -1;
+	while (++y < img.height)
 	{
-		x = 0;
-		while (x < img.width)
+		if (screen_y + y < 0 || screen_y + y >= screen->height)
+			continue ;
+		src_pixel = (int *)(img.data + y * img.size_line);
+		dst_pixel = (int *)(screen->data + (screen_y + y) * screen->size_line + screen_x * pixel_offset);
+		x = -1;
+		while (++x < img.width)
 		{
-			color = img.data + (y * img.size_line + x * (img.bpp / 8));
-			if (*(int *)color >= 0)
-				pixel_put(screen, screen_x + x, screen_y + y, *(int *)color);
-			x++;
+			if (screen_x + x >= 0 && screen_x + x < screen->width && src_pixel[x] >= 0)
+				dst_pixel[x] = src_pixel[x];
 		}
-		y++;
 	}
 }
